bbe2372494001e5cbaacb4261aa7fa83d58ccb61
chh <195822080@qq.com>
2014-11-13 10:01:51 +0800

    创建dao层

---
+++ app/src/main/java/com/hao/money/dao/DatabaseHelper.java
@@ -0,0 +1,31 @@
+package com.hao.money.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * 数据库
+ * Created by Administrator on 2014-11-13.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "mydata.db"; //数据库名称
+    private static final int version = 1; //数据库版本
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //创建用途历史表
+        String sql = "CREATE TABLE `tb_history` (`id`  int(11) NULL AUTO_INCREMENT ,`name`  varchar(225) NULL ,`count`  int(11) NULL ,PRIMARY KEY (`id`));";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+
+    }
+}
