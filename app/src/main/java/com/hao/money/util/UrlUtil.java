cd1bfc0441446703d9576c074507917e613d2345
hao <195822080@qq.com>
2015-01-24 13:03:36 +0800

    修改后台的api接口

--- app/src/main/java/com/hao/money/util/UrlUtil.java
+++ app/src/main/java/com/hao/money/util/UrlUtil.java
@@ -1,19 +1,5 @@
 package com.hao.money.util;
 
-import android.app.Activity;
-import android.content.Context;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import com.hao.money.R;
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-
-import java.math.BigDecimal;
-
 /**
  * 请求地址的工具类
  * Created by hao on 2014/11/2.
