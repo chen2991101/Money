b851fb8b3494a939275ec19b32f09648971b6f92
hao <195822080@qq.com>
2015-01-24 13:03:23 +0800

    修改后台的api接口

--- app/src/main/java/com/hao/money/util/UrlUtil.java
+++ app/src/main/java/com/hao/money/util/UrlUtil.java
@@ -19,8 +19,9 @@ import java.math.BigDecimal;
  * Created by hao on 2014/11/2.
  */
 public class UrlUtil {
-    private static String url = "http://192.168.1.101";//基本的地址
+    //private static String url = "http://192.168.1.101";//基本的地址
+    private static String url = "http://haogupiao.duapp.com/";//外网地址
 
     //上传位置
-    public static String upLoadAddress = url + "/uploadAddress.do";
+    public static String upLoadAddress = url + "/address/uploadAddress.do";
 }
